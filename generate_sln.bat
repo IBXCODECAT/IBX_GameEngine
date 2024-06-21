47c7400a9724293b3fe00ac99dfbf7c022d8f819
IBXCODECAT <nathan.schmitt7@gmail.com>
2024-06-20 20:07:25 -0500

    Fix Premake

--- generate_sln.bat
+++ generate_sln.bat
@@ -1,3 +1,3 @@
 @echo on
-%~dp0vendor/bin/premake/premake5.exe vs2022
+%~dp0vendor/premake/premake5.exe vs2022
 pause
--- premake5.lua
+++ premake5.lua
@@ -12,6 +12,70 @@ outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 ENGINE_NAME = "IBX_Engine"
 
+project "Sandbox"
+    location "Sandbox"
+
+    -- Console Application
+    kind "ConsoleApp"
+
+    language "C++"
+
+    -- Output directory for our binary
+    targetdir ("bin/" ..outputDir .. "/%{prj.name}")
+
+    -- Intermediates output directory
+    objdir ("bin-int/" ..outputDir .. "/%{prj.name}")
+
+    -- Defines the files to include
+    files
+    {
+        -- Recursive search for *.h files in source directory
+        "%{prj.name}/src/**.h",
+
+        -- Recursive search for *.cpp files in source directory
+        "%{prj.name}/src/**.cpp",
+    }
+
+    -- Include directories
+    includedirs
+    {
+        -- Include the source directory (prevents ../../ in includes)
+        "%{prj.name}/src",
+        
+        -- Include spdlog
+        "" .. ENGINE_NAME .. "/vendor/spdlog/include",
+
+        -- Include IBX Engine
+        "IBX_Engine/src/"
+    }
+
+    -- Defines or Preprocessor Directives
+    defines
+    {
+        "IBX_PLATFORM_WINDOWS"
+    }
+
+    -- Link IBX_Engine.dll as a reference
+    links
+    {
+        "IBX_Engine"
+    }
+
+    -- Only applies to the debug configuration (for any platform)
+    filter "configurations:Debug"
+        defines "IBX_DEBUG"
+        symbols "On"
+    
+    -- Only applies to the release configuration (for any platform)
+    filter "configurations:Release"
+        defines "IBX_RELEASE"
+        optimize "On"
+    
+    -- Only applies to the dist configuration (for any platform)
+    filter "configurations:Dist"
+        defines "IBX_DIST"
+        optimize "On"
+        
 project "IBX_Engine"
     location "IBX_Engine"
     
@@ -67,70 +131,6 @@ project "IBX_Engine"
 		{
 			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputDir .. "/Sandbox/\"")
 		}
-
-    -- Only applies to the debug configuration (for any platform)
-    filter "configurations:Debug"
-        defines "IBX_DEBUG"
-        symbols "On"
-    
-    -- Only applies to the release configuration (for any platform)
-    filter "configurations:Release"
-        defines "IBX_RELEASE"
-        optimize "On"
-    
-    -- Only applies to the dist configuration (for any platform)
-    filter "configurations:Dist"
-        defines "IBX_DIST"
-        optimize "On"
-
-project "Sandbox"
-    location "Sandbox"
-
-    -- Console Application
-    kind "ConsoleApp"
-
-    language "C++"
-
-    -- Output directory for our binary
-    targetdir ("bin/" ..outputDir .. "/%{prj.name}")
-
-    -- Intermediates output directory
-    objdir ("bin-int/" ..outputDir .. "/%{prj.name}")
-
-    -- Defines the files to include
-    files
-    {
-        -- Recursive search for *.h files in source directory
-        "%{prj.name}/src/**.h",
-
-        -- Recursive search for *.cpp files in source directory
-        "%{prj.name}/src/**.cpp",
-    }
-
-    -- Include directories
-    includedirs
-    {
-        -- Include the source directory (prevents ../../ in includes)
-        "%{prj.name}/src",
-        
-        -- Include spdlog
-        "" .. ENGINE_NAME .. "/vendor/spdlog/include",
-
-        -- Include IBX Engine
-        "IBX_Engine/src/"
-    }
-
-    -- Defines or Preprocessor Directives
-    defines
-    {
-        "IBX_PLATFORM_WINDOWS"
-    }
-
-    -- Link IBX_Engine.dll as a reference
-    links
-    {
-        "IBX_Engine"
-    }
 
     -- Only applies to the debug configuration (for any platform)
     filter "configurations:Debug"
