1c7946c9444f9f1118bb1cb8a3a495c545aef579
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-06-24 18:58:23 -0500

    Fix Compiler Warnings

--- IBX_Engine/src/IBX/ImGui/ImGuiBuild.cpp
+++ IBX_Engine/src/IBX/ImGui/ImGuiBuild.cpp
@@ -1,10 +1,10 @@
 #include "ibxpch.h"
 
 // Suppress warnings in imgui_impl_opengl3.cpp (We assume that code outside our domain is safe)
-#define _CRT_SECURE_NO_WARNINGS
+// #define _CRT_SECURE_NO_WARNINGS
 
 // Define IMGUI_IMPL_OPENGL_LOADER_GLAD before including imgui_impl_opengl3.cpp to use glad
-//#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+#define IMGUI_IMPL_OPENGL_LOADER_GLAD
 
 #include <backends/imgui_impl_opengl3.cpp>
 #include <backends/imgui_impl_glfw.cpp>
