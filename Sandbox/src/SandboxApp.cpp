8e4857cf5a6f02b7224edb88ad8c029a699d0d64
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-07-02 19:20:08 -0500

    Transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,9 +1,10 @@
 #include <IBX_Engine.h>
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public IBX_Engine::Layer
 {
 public:
-	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
+	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f), m_SquarePosition(0.0f)
 	{
 		m_VertexArray.reset(IBX_Engine::VertexArray::Create());
 
@@ -35,10 +36,10 @@ public:
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
 		float squareVerts[3 * 7] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
 		};
 
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
@@ -67,6 +68,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -76,7 +78,7 @@ public:
 				v_Position = a_Position;
 				v_Color = a_Color;
 
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -104,10 +106,11 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			void main()
 			{
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -160,6 +163,24 @@ public:
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 		}
 
+		if (IBX_Engine::Input::IsKeyPressed(IBX_KEY_I))
+		{
+			m_SquarePosition.y += m_SquareMoveSpeed * ts;
+		}
+		else if (IBX_Engine::Input::IsKeyPressed(IBX_KEY_K))
+		{
+			m_SquarePosition.y -= m_SquareMoveSpeed * ts;
+		}
+
+		if (IBX_Engine::Input::IsKeyPressed(IBX_KEY_J))
+		{
+			m_SquarePosition.x -= m_SquareMoveSpeed * ts;
+		}
+		else if (IBX_Engine::Input::IsKeyPressed(IBX_KEY_L))
+		{
+			m_SquarePosition.x += m_SquareMoveSpeed * ts;
+		}
+
 		IBX_Engine::RenderCommand::SetClearColor({ 1.0f, 0.0f, 1.0f, 1 });
 		IBX_Engine::RenderCommand::Clear();
 
@@ -168,8 +189,19 @@ public:
 
 		IBX_Engine::Renderer::BeginScene(m_Camera);
 
-		IBX_Engine::Renderer::Submit(m_BlueShader, m_SquareVA);
-		IBX_Engine::Renderer::Submit(m_Shader, m_VertexArray);
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePosition);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int i = 0; i < 5; i++)
+		{	
+			glm::vec3 pos(i * 0.11f, 0.0f, 0.0f);
+			glm::mat4 special_transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+
+			IBX_Engine::Renderer::Submit(m_BlueShader, m_SquareVA, special_transform);
+
+		}
+		
+		IBX_Engine::Renderer::Submit(m_Shader, m_VertexArray, transform);
 
 		IBX_Engine::Renderer::EndScene();
 	}
@@ -197,7 +229,12 @@ private:
 	
 	float m_CameraRotation = 0.0f;
 	float m_CameraMoveSpeed = 1.0f;
+
+	float m_SquareMoveSpeed = 1.0f;
+
 	float m_CameraRotationSpeed = 25.0f;
+
+	glm::vec3 m_SquarePosition;
 
 };
 
